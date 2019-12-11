932c804c89666779e353983a32db3d7ae0493c5d
grant744 <55023814+grant744@users.noreply.github.com>
2019-12-11 05:38:48 +0000

    Update

--- configuration/applications/main_application/application_file.php
+++ configuration/applications/main_application/application_file.php
@@ -2,12 +2,14 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Тематический сайт</title>
+        <title>Информационный ресурс</title>
         <link rel="stylesheet" href="configuration/applications/main_application/css/style.css">
         <?=$jquery->use(); ?>
     </head>
 
     <body>
-        <div class="message">Welcome to the home page </div>
+        <div class="frame">
+			<div class="welcomeBlock"></div>
+		</div>
     </body>
 </html>
